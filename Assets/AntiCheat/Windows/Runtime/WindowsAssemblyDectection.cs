caf5a817bc6bbe6bcee373fb80951f74be4e5f67
cqtd <cqtd@protonmail.ch>
2020-07-18 23:48:19 +0900

    윈도우 어셈블리 로드 디텍터

---
+++ Assets/AntiCheat/Windows/Runtime/WindowsAssemblyDectection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+namespace CQ.AntiCheat
+{
+	namespace Windows
+	{
+		public class WindowsAssemblyDectection : WindowsBase
+		{
+			public bool registerOnAwake = false;
+			
+			bool isRegistered;
+			
+			void Awake()
+			{
+				if (registerOnAwake)
+				{
+					Register();
+				}
+				
+				
+			}
+
+			void OnDestroy()
+			{
+				Unregister();
+			}
+
+			void OnApplicationQuit()
+			{
+				Unregister();
+			}
+
+			void OnAssemblyLoad(object sender, AssemblyLoadEventArgs assemblyLoadEventArgs)
+			{
+				Debug.Log($"[Windows][AssemblyLoaded]::{assemblyLoadEventArgs.LoadedAssembly.FullName}");
+				
+				// filter here
+			}
+
+			void Register()
+			{
+				if (isRegistered)
+				{
+					Debug.Log("Already Registered.");
+					return;
+				}
+
+				AppDomain.CurrentDomain.AssemblyLoad += OnAssemblyLoad;
+				isRegistered = true;
+			}
+
+			void Unregister()
+			{
+				if (!isRegistered)
+				{
+					Debug.Log("Not Registered yet.");
+					return;
+				}
+				
+				AppDomain.CurrentDomain.AssemblyLoad -= OnAssemblyLoad;
+				isRegistered = false;
+			}
+		}
+	}
+}
